08e42e7fa93209be0084a91b938bed4320ca069b
Kaan Acikgoz <kaanacikgoz@outlook.com>
2024-07-08 16:18:45 +0300

    Created BookBorrowing Entity

---
+++ src/main/java/com/kaanAcikgoz/BookBorrowing.java
@@ -0,0 +1,61 @@
+package com.kaanAcikgoz;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "bookBorrowings")
+public class BookBorrowing {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "bookBorrow_id",columnDefinition = "serial")
+    private int id;
+
+    @Column(name = "borrower_name",nullable = false)
+    private String borrowerName;
+
+    @Column(name = "borrowing_date",nullable = false)
+    private LocalDate borrowingDate;
+
+    @Column(name = "borrow_returnDate",nullable = false)
+    private LocalDate returnDate;
+
+    public BookBorrowing() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getBorrowerName() {
+        return borrowerName;
+    }
+
+    public void setBorrowerName(String borrowerName) {
+        this.borrowerName = borrowerName;
+    }
+
+    public LocalDate getBorrowingDate() {
+        return borrowingDate;
+    }
+
+    public void setBorrowingDate(LocalDate borrowingDate) {
+        this.borrowingDate = borrowingDate;
+    }
+
+    public LocalDate getReturnDate() {
+        return returnDate;
+    }
+
+    public void setReturnDate(LocalDate returnDate) {
+        this.returnDate = returnDate;
+    }
+
+}
