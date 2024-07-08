b705dce6596cbf8e182e962b341a6a0d1336c6fb
Kaan Acikgoz <kaanacikgoz@outlook.com>
2024-07-08 15:36:48 +0300

    Created Main Class and Book Entity

---
+++ src/main/java/com/kaanAcikgoz/Book.java
@@ -0,0 +1,59 @@
+package com.kaanAcikgoz;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "books")
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "book_id",columnDefinition = "serial")
+    private int id;
+
+    @Column(name = "book_name",unique = true,nullable = false)
+    private String name;
+
+    @Column(name = "book_publicationYear",nullable = false)
+    private int publicationYear;
+
+    @Column(name = "book_stock")
+    private int stock;
+
+    public Book() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+
+    public void setPublicationYear(int publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+}
